fd79bafb98bd1a0748c9e204589aef4d27873216
Arath Cabral <censedgiraffed96@gmail.com>
2025-10-08 17:08:45 -0600

    Add files via upload

---
+++ EVA2_14_ARREGLOS_FOR/src/eva2_14_arreglos_for/EVA2_14_ARREGLOS_FOR.java
@@ -0,0 +1,19 @@
+
+package eva2_14_arreglos_for;
+
+public class EVA2_14_ARREGLOS_FOR {
+
+    public static void main(String[] args) {
+       int arreglo[] = new int [10];
+       
+       
+       //System.out.println((int)(Math.random() * 100));
+       for(int f = 0; f < arreglo.length; f++)
+           arreglo[f] = (int)(Math.random() * 100);
+       
+       for(int f = 0; f < arreglo.length; f++)
+          System.out.print("[" + arreglo [f] + "]");
+      
+    }
+
+}
